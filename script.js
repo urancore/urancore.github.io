0386338a10bf4d8f02adf0b4e5c275b052be56b5
pydragon <137177056+pydragon1@users.noreply.github.com>
2025-04-16 21:18:05 +0700

    upd

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ function showAd() {
 function closeAd() {
 	document.getElementById("adPopup").style.display = "none";
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+	const avatar = document.getElementById('avatar');
+	let isChanged = false;
+
+	// Предзагрузка нового изображения
+	const newImage = new Image();
+	newImage.src = 'assets/b.avif'; // Укажите правильный путь
+
+	avatar.addEventListener('mouseover', function () {
+		if (isChanged) return;
+
+		avatar.style.transform = 'rotate(720deg)';
+		avatar.addEventListener('transitionend', function handler(e) {
+			if (e.propertyName === 'transform') {
+				avatar.removeEventListener('transitionend', handler);
+				avatar.src = newImage.src;
+				avatar.style.transform = 'none'; // Сброс вращения
+				isChanged = true;
+			}
+		});
+	});
+});
